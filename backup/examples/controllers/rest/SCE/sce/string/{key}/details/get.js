679ab43fb999fea73c2161c13a0dcfe631f4698f
VinayK93 <vinaykumar7737245904@gmail.com>
2018-02-24 13:05:16 +0530

    Resolve library

--- backup/examples/controllers/rest/SCE/sce/string/{key}/details/get.js
+++
@@ -1,8 +0,0 @@
-/*jslint node: true */
-                /*jshint globalstrict: true*/
-'use strict';
-exports.handlers = function (event, context, callback) {
-    console.log(event); //parameter passed in it
-    console.log(context);
-    callback(null, '{"statusCode":400,"message":"this is the response"}');
-};
